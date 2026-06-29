add1e875e82906d825a43ffd6d66308e7c39a18d
conor <conor@Aragorn>
2026-06-28 21:23:47 -0600

    Commit #1 for Main

--- StudentArrayListSelectionSort/src/studentarraylist/Main.java
+++ StudentArrayListSelectionSort/src/studentarraylist/Main.java
@@ -1,9 +1,27 @@
 package studentarraylist;
 
+
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ArrayList<Student> students = new ArrayList<>();
+		
+		
+		students.add(new Student(105, "Conor", "De Laurell"));
+		students.add(new Student(109, "John", "Denver"));
+		students.add(new Student(103, "Christopher", "Cross"));
+		students.add(new Student(102, "Jim", "Croce"));
+		students.add(new Student(107, "Michael", "De Laurell"));
+		students.add(new Student(101, "Michael", "McDonald"));
+		students.add(new Student(108, "Elijah", "Wood"));
+		students.add(new Student(111, "Sean", "Astin"));
+		students.add(new Student(116, "Billy", "Boyd"));
+		students.add(new Student(112, "Dominic", "Monaghan"));
+		students.add(new Student(132, "Viggo", "Mortensen"));
+		
 
 	}
 
