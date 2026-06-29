9d15607dea8f76652a0ae00db90e3b15ea0f789f
conor <conor@Aragorn>
2026-06-28 21:07:00 -0600

    Commit for NameComparator

--- StudentArrayListSelectionSort/src/studentarraylist/NameComparator.java
+++ StudentArrayListSelectionSort/src/studentarraylist/NameComparator.java
@@ -1,5 +1,18 @@
 package studentarraylist;
 
-public class NameComparator {
+
+import java.util.Comparator;
+
+public class NameComparator implements Comparator<Student>{
+	
+	
+	
+	@Override
+	public int compare(Student s1, Student s2) {
+		
+		
+		return s1.getName().compareToIgnoreCase(s2.getName());
+		
+	}
 
 }
